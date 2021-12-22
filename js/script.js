2312cc7d0b48f47fd8cf12fb825aa8eaad2715de
GiandoSaba <giandosaba997@gmail.com>
2021-12-22 16:14:51 +0100

    message-menu

--- js/script.js
+++ js/script.js
@@ -15,16 +15,19 @@ const app = new Vue({
                         date: "10/01/2020 15:30:55",
                         text: "Hai portato a spasso il cane?",
                         status: "sent",
+                        hover: false,
                     },
                     {
                         date: "10/01/2020 15:50:00",
                         text: "Ricordati di dargli da mangiare",
                         status: "sent",
+                        hover: false,
                     },
                     {
                         date: "10/01/2020 16:15:22",
                         text: "Tutto fatto!",
                         status: "received",
+                        hover: false,
                     },
                 ],
             },
@@ -38,16 +41,19 @@ const app = new Vue({
                         date: "20/03/2020 16:30:00",
                         text: "Ciao come stai?",
                         status: "sent",
+                        hover: false,
                     },
                     {
                         date: "20/03/2020 16:30:55",
                         text: "Bene grazie! Stasera ci vediamo?",
                         status: "received",
+                        hover: false,
                     },
                     {
                         date: "20/03/2020 16:35:00",
                         text: "Mi piacerebbe ma devo andare a fare la spesa.",
                         status: "sent",
+                        hover: false,
                     },
                 ],
             },
@@ -62,16 +68,19 @@ const app = new Vue({
                         date: "28/03/2020 10:10:40",
                         text: "La Marianna va in campagna",
                         status: "received",
+                        hover: false,
                     },
                     {
                         date: "28/03/2020 10:20:10",
                         text: "Sicuro di non aver sbagliato chat?",
                         status: "sent",
+                        hover: false,
                     },
                     {
                         date: "28/03/2020 16:15:22",
                         text: "Ah scusa!",
                         status: "received",
+                        hover: false,
                     },
                 ],
             },
@@ -85,11 +94,13 @@ const app = new Vue({
                         date: "10/01/2020 15:30:55",
                         text: "Lo sai che ha aperto una nuova pizzeria?",
                         status: "sent",
+                        hover: false,
                     },
                     {
                         date: "10/01/2020 15:50:00",
                         text: "Si, ma preferirei andare al cinema",
                         status: "received",
+                        hover: false,
                     },
                 ],
             },
@@ -110,7 +121,8 @@ const app = new Vue({
             'Le mie fonti dicono di no',
             'Le prospettive non sono buone',
             'Molto incerto'
-        ]
+        ],
+        messageSent: false,
     },
     created() {
         this.active = 0;
@@ -136,22 +148,26 @@ const app = new Vue({
                 {
                     date: '',
                     text: '',
-                    status: "sent"
+                    status: "sent",
+                    hover: false
                 }
                 let now = dayjs();
                 const data = `${now.format('HH')}:${now.format('mm')}`
                 newMessage.date = data;
                 newMessage.text = message;
                 this.contacts[active].messages.push(newMessage);
-                setTimeout(() => {
-                    this.contacts[active].lastAccess = 'online'
+                if (!this.messageSent) {
                     setTimeout(() => {
-                        this.contacts[active].lastAccess = 'sta digitando..'
+                        this.contacts[active].lastAccess = 'online'
                         setTimeout(() => {
-                            this.receiveNewMessage(active);
-                        }, 1000);
+                            this.contacts[active].lastAccess = 'sta digitando..'
+                            setTimeout(() => {
+                                this.receiveNewMessage(active);
+                            }, 1000);
+                        }, 2000);
                     }, 2000);
-                }, 2000);
+                    this.messageSent = true;
+                }
 
             }
         },
@@ -161,7 +177,8 @@ const app = new Vue({
             {
                 date: '',
                 text: '',
-                status: "received"
+                status: "received",
+                hover: false
             }
             let now = dayjs();
             const data = `${now.format('HH')}:${now.format('mm')}`;
@@ -174,6 +191,7 @@ const app = new Vue({
             setTimeout(() => {
                 const now = dayjs();
                 this.contacts[active].lastAccess = `Ultimo accesso oggi alle ${now.format('HH')}:${now.format('mm')}`;
+                this.messageSent = false;
             }, 3000);
         },
 
